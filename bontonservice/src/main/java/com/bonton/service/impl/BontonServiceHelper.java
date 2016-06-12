a84147b3a51fae56581055fa005ed095bb844fb1
Tirath <tirath.rawat@hotmail.com>
2016-06-12 08:44:55 +0530

    Bonton service helper

---
+++ bontonservice/src/main/java/com/bonton/service/impl/BontonServiceHelper.java
@@ -0,0 +1,20 @@
+package com.bonton.service.impl;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.bonton.service.ServiceProxy;
+
+public class BontonServiceHelper {
+	private static final ExecutorService es = Executors.newCachedThreadPool();
+	public void execute(ServiceProxy sp) {
+		//final CountDownLatch cdl = new CountDownLatch(3);
+		Future<Boolean> result = es.submit(new Runnable() {
+			@Override
+			public void run() {
+				//sp.search(null, null, null);
+			}}, true);
+		
+	}
+}
