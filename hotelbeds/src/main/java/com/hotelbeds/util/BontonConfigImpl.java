ed8fb1603870e6f79f90e9c6057395f9312b2885
Tirath <tirath.rawat@hotmail.com>
2016-06-16 16:29:01 +0530

    Removed unwanted code.

--- hotelbeds/src/main/java/com/hotelbeds/util/BontonConfigImpl.java
+++ hotelbeds/src/main/java/com/hotelbeds/util/BontonConfigImpl.java
@@ -1,7 +1,5 @@
 package com.hotelbeds.util;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -18,9 +16,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class BontonConfigImpl {
 	
 	private static OkHttpClient restTemplate = null;
-	private static int readTimeout = BontonProperties.REST_TEMPLATE_READ_TIME_OUT;
-    private static int connectTimeout = BontonProperties.REST_TEMPLATE_READ_TIME_OUT;
-    private static int connectionRequestTimeout = BontonProperties.REST_TEMPLATE_READ_TIME_OUT;
+	private static int readTimeout = HBProperties.REST_TEMPLATE_READ_TIME_OUT;
+    private static int connectTimeout = HBProperties.REST_TEMPLATE_READ_TIME_OUT;
+    private static int connectionRequestTimeout = HBProperties.REST_TEMPLATE_READ_TIME_OUT;
 	private static boolean initialised = false;
 	private static ExecutorService executorService = null;
     private static ObjectMapper mapper = null;
@@ -46,22 +44,6 @@ public class BontonConfigImpl {
 		}
     }
     
-    /**
-     * This method is used to check which points are enabled out of Desia , Expedia 
-     * @return Map of Enabled End Points with value true or false
-     */
-    public static Map<String,Boolean> getEnabledEndPoints() {
-		
-    	Map<String,Boolean> resultMap = new HashMap<String,Boolean>();
-    	
-    	resultMap.put(BontonProperties.HB_HOTEL_API_SERVICE, new Boolean(true));
-    	resultMap.put(BontonProperties.EXPEDIA_HOTEL_API_SERVICE, new Boolean(false));
-    	resultMap.put(BontonProperties.DESIA_HOTEL_API_SERVICE, new Boolean(false));
-    	
-		return resultMap;
-		
-    }
-	
 	public static OkHttpClient getRestTemplate() {
 		return restTemplate;
 	}
