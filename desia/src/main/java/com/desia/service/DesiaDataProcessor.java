788369a4e8c90c71c106f32364deb168c64948e3
Tirath <tirath.rawat@hotmail.com>
2016-06-19 13:04:53 +0530

    Testing new hbclient branch

--- desia/src/main/java/com/desia/service/DesiaDataProcessor.java
+++ desia/src/main/java/com/desia/service/DesiaDataProcessor.java
@@ -1,15 +1,5 @@
 package com.desia.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.bonton.utility.desia.Hotels;
-import com.desia.artifacts.OTAHotelAvailRS;
-import com.desia.artifacts.RatePlanType;
-import com.desia.artifacts.RoomStayType.RoomRates.RoomRate;
-import com.desia.artifacts.RoomTypeType;
-
 public class DesiaDataProcessor {
 		
 }
